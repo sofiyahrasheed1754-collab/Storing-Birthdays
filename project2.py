1f961bbf4e0c4809c234b53ca4afd9c631ec52a5
sofiyahrasheed1754-collab <sofiyahrasheed1754@gmail.com>
2026-07-23 18:07:58 +0100

    1st

---
+++ project2.py
@@ -0,0 +1,16 @@
+print("Birthday of Rajs' friends")
+name="Ayesha"
+birth_day="2012-07-03"
+print(name, birth_day)
+name="Kamil"
+birth_day="2010-11-21"
+print(name, birth_day)
+name="Ihsan"
+birth_day="2011-05-15"
+print(name, birth_day)
+name="Ayaan"
+birth_day="2013-09-12"
+print(name, birth_day)
+name="Orhan"
+birth_day="2011-10-03"
+print(name, birth_day)
